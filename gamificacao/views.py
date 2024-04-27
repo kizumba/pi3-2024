a97bb59265b7e968cc728ffdffd22a8ca8a3d923
kizumba <12.25andre@gmail.com>
2024-04-27 13:58:46 -0300

    Base.html adicionada

--- gamificacao/views.py
+++ gamificacao/views.py
@@ -5,3 +5,6 @@ from .models import Equipe
 def index(request):
     equipes = Equipe.objects.all()
     return render(request, 'index.html',{'equipes':equipes})
+
+def teste(request):
+    return render(request, 'teste.html',{})
