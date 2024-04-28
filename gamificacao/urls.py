2f0e365a0b17928557fc9828d7ae13ba5e472354
kizumba <12.25andre@gmail.com>
2024-04-28 19:46:17 -0300

    Adicionado Esqueleto do site

--- gamificacao/urls.py
+++ gamificacao/urls.py
@@ -5,10 +5,16 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),    
     path('turmas', views.turmas, name='turmas'),
-    path('equipes/', views.equipes, name='equipes'),
-    path('atitudes/', views.atitudes, name='atitudes'),
-    path('missoes/', views.missoes, name='missoes'),
+    path('equipes', views.equipes, name='equipes'),
+    path('atitudes', views.atitudes, name='atitudes'),
+    path('missoes', views.missoes, name='missoes'),
     path('ajuda', views.ajuda, name='ajuda'),
-    path('sobre/', views.sobre, name='sobre'),
+    path('sobre', views.sobre, name='sobre'),
     path('aula', views.aula, name='aula'),
+
+    # telas de cadastro
+    path('cadastrarturmas', views.cadastrar_turmas, name='cadastrarturmas'),
+    path('cadastrarequipes', views.cadastrar_equipes, name='cadastrarequipes'),
+    path('cadastraratitudes',views.cadastrar_atitudes, name='cadastraratitudes'),
+    path('cadastrarmissoes',views.cadastrar_missoes, name='cadastrarmissoes')
 ]
--- gamificacao/views.py
+++ gamificacao/views.py
@@ -1,24 +1,41 @@
 from django.shortcuts import render
 
-from .models import Equipe
+from .models import Equipe,Turma, Atitude, Missao
 
 
 # Funções para chamar as páginas html
 def index(request):
+    turmas = Turma.objects.all()
     equipes = Equipe.objects.all()
-    return render(request, 'index.html',{'equipes':equipes})
+    atitudes = Atitude.objects.all()
+    missoes = Missao.objects.all()
+
+    return render(request, 'index.html',{
+            'turmas':turmas,
+            'equipes':equipes,
+            'atitudes':atitudes,
+            'missoes':missoes
+        })
 
 def turmas(request):
-    return render(request, 'turmas.html',{})
+    turmas = Turma.objects.all()
+
+    return render(request, 'turmas.html',{'turmas':turmas})
 
 def equipes(request):
-    return render(request, 'equipes.html', {})
+    equipes = Equipe.objects.all()
+
+    return render(request, 'equipes.html', {'equipes':equipes})
 
 def atitudes(request):
-    return render(request, 'atitudes.html', {})
+    atitudes = Atitude.objects.all()
+
+    return render(request, 'atitudes.html', {'atitudes':atitudes})
 
 def missoes(request):
-    return render(request, 'missoes.html', {})
+    missoes = Missao.objects.all()
+
+    return render(request, 'missoes.html', {'missoes':missoes})
 
 def ajuda(request):
     return render(request, 'ajuda.html', {})
@@ -28,3 +45,19 @@ def sobre(request):
 
 def aula(request):
     return render(request, 'aula.html',{})
+
+
+# CADASTROS
+def cadastrar_turmas(request):
+    turma = Turma
+
+    return render(request, 'cadastrarturmas.html')
+
+def cadastrar_equipes(request):
+    return render(request, 'cadastrarequipes.html')
+
+def cadastrar_atitudes(request):
+    return render(request, 'cadastraratitudes.html')
+
+def cadastrar_missoes(request):
+    return render(request, 'cadastrarmissoes.html')
